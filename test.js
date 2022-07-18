7545a5c1fa38afd0158a0de291ee528ea7660b50
Soldy <4786022+Soldy@users.noreply.github.com>
2022-07-18 08:56:03 +0100

    error message extension
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -8,16 +8,124 @@ const _runner_class =  (require('./index.js')).base;
 let level_one = false;
 let level_before = false;
 let level_after = false;
-
-const runner =  new _runner_class(
-    function(){
-        level_before = true;
+let runner;
+(async function(){
+nanoTest.add(
+    'init defore not a function',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                593,
+                function(){
+                    level_after = true;
+                },
+                'cool'
+            );
+        }
     },
-    function(){
-        level_after = true;
-    },
-    3
+    'error'
 );
+nanoTest.add(
+    'init after not a function',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                593,
+                'cool'
+            );
+        }
+    },
+    'error'
+);
+nanoTest.add(
+    'init cool level',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                function(){
+                    level_after = true;
+                },
+                'cool'
+            );
+        }
+    },
+    'error'
+);
+nanoTest.add(
+    'init 1.5 level',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                function(){
+                    level_after = true;
+                },
+                1.5
+            );
+        }
+    },
+    'error'
+);
+nanoTest.add(
+    'init 0 level',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                function(){
+                    level_after = true;
+                },
+                0
+            );
+        }
+    },
+    'error'
+);
+nanoTest.add(
+    'init to much level',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                function(){
+                    level_after = true;
+                },
+                101
+            );
+        }
+    },
+    'error'
+);
+nanoTest.add(
+    'init',
+    {
+        'function' : function(){
+            runner =  new _runner_class(
+                function(){
+                    level_before = true;
+                },
+                function(){
+                    level_after = true;
+                },
+                3
+            );
+        }
+    },
+    '!error'
+);
+await nanoTest.partly();
 nanoTest.add(
     'level_one status',
     {
@@ -66,6 +174,61 @@ nanoTest.add(
     true
 );
 nanoTest.add(
+    'add string as a function to level one',
+    {
+        'function':runner.add,
+        'options' :[
+            'not a function',
+            1
+        ]
+    },
+    'error'
+);
+nanoTest.add(
+    'add string as a function to level -1',
+    {
+        'function':runner.add,
+        'options' :[
+            ()=>{},
+            -1
+        ]
+    },
+    'error'
+);
+nanoTest.add(
+    'add string as a function to level cool',
+    {
+        'function':runner.add,
+        'options' :[
+            ()=>{},
+            'cool'
+        ]
+    },
+    'error'
+);
+nanoTest.add(
+    'add string as a function to level 2.5',
+    {
+        'function':runner.add,
+        'options' :[
+            ()=>{},
+            2.5
+        ]
+    },
+    'error'
+);
+nanoTest.add(
+    'add string as a function to level 4',
+    {
+        'function':runner.add,
+        'options' :[
+            ()=>{},
+            4
+        ]
+    },
+    'error'
+);
+nanoTest.add(
     'run runner',
     {
         'function':function(){runner.run();return true;},
@@ -107,3 +270,4 @@ nanoTest.add(
 );
 
 nanoTest.run();
+})();
